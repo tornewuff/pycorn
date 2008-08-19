7fc54da61155a73e7669b7c07c0b04afbdd52b82
Torne Wuff <torne@wolfpuppy.org.uk>
2008-08-19 18:52:02 +0100

    Add support for building flash images, and running them under QEMU, with docs.

--- plat/arm/pxa270/pxa270.mk
+++ plat/arm/pxa270/pxa270.mk
@@ -1,3 +1,14 @@
 CFLAGS += -mcpu=xscale -mtune=xscale
 KERNELMKIMAGE += -a 0xa0008000 -e 0xa0008000
 BOARDOBJECTS := serial.o
+
+$(phony run): $(notdir $(CURDIR)).flash
+	@&echo You can exit qemu with C-a x (C-a a x under screen)
+	qemu-system-arm -M verdex -pflash $(input) -nographic -m 289
+
+%.flash: %.uimage $(BOARDPATH)/u-boot.bin $(BOARDPATH)/u-boot.env
+	&rm -f $(output)
+	dd of=$(output) bs=4k count=4k if=/dev/zero
+	dd of=$(output) bs=4k conv=notrunc if=$(BOARDPATH)/u-boot.bin
+	dd of=$(output) bs=4k conv=notrunc seek=63 if=$(BOARDPATH)/u-boot.env
+	dd of=$(output) bs=4k conv=notrunc seek=64 if=$(input)
