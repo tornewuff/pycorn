4ac3200ab2b53e3ea8f0fb4f3dcaadc690198cc4
Torne Wuff <torne@wolfpuppy.org.uk>
2008-08-21 13:55:38 +0100

    Split getpath stuff into a seperate file

---
+++ pykern/getpath.c
@@ -0,0 +1,4 @@
+char *Py_GetPath(){ return "/"; }
+char *Py_GetPrefix() { return "/"; }
+char *Py_GetExecPrefix() { return "/"; }
+char *Py_GetProgramFullPath() { return "/python"; }
--- pykern/main.c
+++ pykern/main.c
@@ -2,11 +2,6 @@
 
 extern int Py_Main(int, char*[]);
 
-char *Py_GetPath(){ return "/"; }
-char *Py_GetPrefix() { return "/"; }
-char *Py_GetExecPrefix() { return "/"; }
-char *Py_GetProgramFullPath() { return "/python"; }
-
 //char *pyargs[] = { "python", "-c", "import sys\nwhile True: print 'You pressed %s' % (ord(sys.stdin.read(1)))", NULL };
 char *pyargs[] = { "python", "-v", "-S", "-i", NULL };
 
