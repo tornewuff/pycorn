6d0ffec3b24da6c2cf5123c49a9b5887eb911ea5
Torne Wuff <torne@wolfpuppy.org.uk>
2008-09-10 00:14:17 +0100

    Move library inclusion stuff into common.mk

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
-# Disable makepp's builtin link rules as they're not particularly cross-compile-y
-makepp_no_builtin_linker = 1
+# Disable makepp's builtin rules as they're not particularly cross-compile-y
+makepp_no_builtin = 1
 
 # Target selection, stuck here for now
 ARCH := arm
@@ -14,12 +14,14 @@ MKIMAGE := mkimage
 # Flags and paths
 ARCHPATH := $(ROOT)/plat/$(ARCH)
 BOARDPATH := $(ROOT)/plat/$(ARCH)/$(BOARD)
-CFLAGS := -g -Wall -O2 -fomit-frame-pointer -I$(ARCHPATH) -I$(BOARDPATH)
+CFLAGS := -g -Wall -O2 -fomit-frame-pointer
+CPPFLAGS := -I$(ARCHPATH) -I$(BOARDPATH) -I$(ROOT)/libs/$( $(dir $(LIBS)))include
 PYCFLAGS := -fomit-frame-pointer -Werror -Wno-error=strict-aliasing -Wno-error=char-subscripts
 LDFLAGS := -B$(BOARDPATH) -B$(ARCHPATH) -specs=$(BOARD).specs
 LDDEPS := $(BOARDPATH)/$(BOARD)$( .specs .ld) $(ARCHPATH)/crt0.o
 MKIMAGEFLAGS := -A $(ARCH) -O linux
 KERNELMKIMAGE := $(MKIMAGEFLAGS) -T kernel
+LIBOBJECTS := $(ROOT)/libs/$( $(LIBS))
 
 # Get board-specific stuff
 include $(ROOT)/plat/$(ARCH)/$(ARCH).mk
@@ -34,9 +36,13 @@ ifdef OBJECTS
 %.bin: %.elf
 	$(OBJCOPY) -O binary $(input) $(output)
 
-$(notdir $(CURDIR)).elf: $(OBJECTS) $(BOARDPATH)/$( $(BOARDOBJECTS)) $(ARCHPATH)/$( $(ARCHOBJECTS)) $(LDDEPS)
-	$(CC) $(OBJECTS) $(BOARDPATH)/$( $(BOARDOBJECTS)) $(ARCHPATH)/$( $(ARCHOBJECTS)) $(LDFLAGS) $(LIBS) -o $(output)
+$(notdir $(CURDIR)).elf: $(OBJECTS) $(BOARDPATH)/$( $(BOARDOBJECTS)) $(ARCHPATH)/$( $(ARCHOBJECTS)) $(LIBOBJECTS) $(LDDEPS)
+	$(CC) $(OBJECTS) $(BOARDPATH)/$( $(BOARDOBJECTS)) $(ARCHPATH)/$( $(ARCHOBJECTS)) $(LDFLAGS) $(LIBOBJECTS) $(SYSLIBS) -o $(output)
 endif
+
+# we disable the scanner because it causes problems with the dynamically created includes from our libs
+%.o: %.c $(ROOT)/libs/$( $(dir $(LIBS)))stamp-include : scanner none
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $(input) -o $(output)
 
 %.o: %.S
 	$(CC) $(ASFLAGS) $(CPPFLAGS) -c $(input) -o $(output)
