2fa002a0a0f0ef753ed9ae1371eba8937d51c5de
Torne Wuff <torne@wolfpuppy.org.uk>
2008-10-12 02:13:41 +0100

    Minimal bootloader taglist parsing

---
+++ pykern/frozen/pycorn/bootldr.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2008 Torne Wuff
+#
+# This file is part of Pycorn.
+#
+# Pycorn is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+
+import struct
+
+import _bootldr
+import metal
+
+def _tagdata():
+    header = struct.Struct('2I')
+    addr = _bootldr.taglist_addr
+    while True:
+        headerbuf = metal.membuf(addr, header.size)
+        (size, tag) = header.unpack(headerbuf)
+        if tag == 0:
+            break
+        data = metal.membuf(addr + header.size, size * 4 - header.size)
+        yield tag, data
+        addr += size * 4
+
+_tagtypes = {
+        0x54410001: ('core', struct.Struct('3I'),
+            ('flags', 'pagesize', 'rootdev')),
+        0x54410002: ('mem32', struct.Struct('2I'),
+            ('size', 'start')),
+        0x54410006: ('serialnr', struct.Struct('Q'),
+            ('serialnr',)),
+        }
+
+def taglist():
+    for tag, data in _tagdata():
+        (name, parser, fields) = _tagtypes[tag]
+        values = parser.unpack(data)
+        tagdict = dict(zip(fields, values))
+        yield name, tagdict
