3d623284068d8b039011f393c50a2254857966bf
Torne Wuff <torne@wolfpuppy.org.uk>
2009-09-17 21:06:05 +0100

    Add support for booting with a ramdisk

--- arch/arm/arm.mk
+++ arch/arm/arm.mk
@@ -4,8 +4,14 @@ PYMODULES += $(ARCHPATH)/pymodules/$( _bootldrmodule.o _metalcpu_coproc.o _metal
 MKIMAGE := mkimage
 MKIMAGEFLAGS := -A arm -O linux
 KERNELMKIMAGE := $(MKIMAGEFLAGS) -T kernel
+INITRDMKIMAGE := $(MKIMAGEFLAGS) -T ramdisk
 
 ifdef OBJECTS
 %.uimage: %.bin
-       $(MKIMAGE) -C none $(KERNELMKIMAGE) -n $(basename $(input)) -d $(input) $(output)
+	$(MKIMAGE) -C none $(KERNELMKIMAGE) -n $(basename $(input)) -d $(input) $(output)
+
+ifdef RAMDISK
+$(RAMDISK).uimage: $(RAMDISK)
+	$(MKIMAGE) -C none $(INITRDMKIMAGE) -n $(input) -d $(input) $(output)
+endif
 endif
