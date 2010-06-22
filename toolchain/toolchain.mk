eb50104361a629e82180daaa3064cae621bc79cc
Torne Wuff <torne@wolfpuppy.org.uk>
2010-06-22 20:08:59 +0100

    Fix up toolchain-making recipie to produce proper system header files
    You need to make a new toolchain if you had one built the old way!

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -2,4 +2,20 @@ CFLAGS := -O2 -pipe
 LDFLAGS :=
 TOOLSTARGET := arm-eabi
 
+BINUTILSVER := 2.19.1
+BINUTILSTAR := binutils-$(BINUTILSVER).tar.bz2
+BINUTILSDIR := binutils-$(BINUTILSVER)
+
+GCCVER := 4.4.1
+GCCTAR := gcc-core-$(GCCVER).tar.bz2
+GCCDIR := gcc-$(GCCVER)
+
+GDBVER := 6.8
+GDBTAR := gdb-$(GDBVER).tar.bz2
+GDBDIR := gdb-$(GDBVER)
+
+NEWLIBVER := 1.17.0
+NEWLIBTAR := newlib-$(NEWLIBVER).tar.gz
+NEWLIBDIR := newlib-$(NEWLIBVER)
+
 export PATH := $(TOOLSPREFIX)/bin:$(PATH)
