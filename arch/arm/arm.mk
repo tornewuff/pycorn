9779ff79a359be36f00527d887231c6c522b3e61
Torne Wuff <torne@wolfpuppy.org.uk>
2010-12-08 17:08:30 +0000

    Comment -fnodwarf2-cfi-asm with an explanation of *why* it's needed

--- arch/arm/arm.mk
+++ arch/arm/arm.mk
@@ -1,5 +1,10 @@
+# binutils 2.19 generates a loadable .eh_frame section from .cfi directives
+# even though it should be generating a non-loadable .debug_frame section.
+# so, until we are using a newer binutils, add this option to force gcc to
+# generate the unwinding info itself, which it does right
 CFLAGS += -fno-dwarf2-cfi-asm
 PYCFLAGS += -fno-dwarf2-cfi-asm
+
 EXTMODULES += $(ARCHPATH)/metal/ext/$( _bootstrapmodule.o _metalcpu_coproc.o _metalcpumodule.o)
 FREEZEDIRS += $(ARCHPATH)/metal/frozen
 INITRDDIRS += $(ARCHPATH)/metal/initrd
