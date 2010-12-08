f36a865cfb29882351a338cfe26cedbd1b87aa56
Torne Wuff <torne@wolfpuppy.org.uk>
2010-12-08 20:20:06 +0000

    Move image building and u-boot config into embryo machine directory

--- arch/arm/machine/pxa270/pxa270.mk
+++ arch/arm/machine/pxa270/pxa270.mk
@@ -1,25 +1,2 @@
 CFLAGS += -mcpu=xscale -mtune=xscale
 PYCFLAGS += -mcpu=xscale -mtune=xscale
-
-ifdef OBJECTS
-$(phony run): $(notdir $(CURDIR)).flash
-	@&echo You can exit qemu with C-a x (C-a a x under screen)
-	qemu-system-arm -M verdex -pflash $(input) -nographic -m 289
-
-ifdef RAMDISK
-%.flash: %.uimage $(ROOT)/downloads/u-boot-verdex-600-r1604.bin $(MACHPATH)/u-boot-ramdisk.env $(RAMDISK).uimage
-	&rm -f $(output)
-	dd of=$(output) bs=4k count=8k if=/dev/zero
-	dd of=$(output) bs=4k conv=notrunc if=$(ROOT)/downloads/u-boot-verdex-600-r1604.bin
-	dd of=$(output) bs=4k conv=notrunc seek=63 if=$(MACHPATH)/u-boot-ramdisk.env
-	dd of=$(output) bs=4k conv=notrunc seek=64 if=$(input)
-	dd of=$(output) bs=4k conv=notrunc seek=1024 if=$(RAMDISK).uimage
-else
-%.flash: %.uimage $(ROOT)/downloads/u-boot-verdex-600-r1604.bin $(MACHPATH)/u-boot.env
-	&rm -f $(output)
-	dd of=$(output) bs=4k count=8k if=/dev/zero
-	dd of=$(output) bs=4k conv=notrunc if=$(ROOT)/downloads/u-boot-verdex-600-r1604.bin
-	dd of=$(output) bs=4k conv=notrunc seek=63 if=$(MACHPATH)/u-boot.env
-	dd of=$(output) bs=4k conv=notrunc seek=64 if=$(input)
-endif
-endif
--- libs/embryo/arm/pxa270/embryo.mk
+++ libs/embryo/arm/pxa270/embryo.mk
@@ -1,2 +1,27 @@
 LDDEPS += $(EMBRYODIR)/$(ARCH)/$(ARCH)-mmu-single.ld
 KERNELMKIMAGE += -a 0xa0008000 -e 0xa0008000
+
+ifdef MAKING_SEED
+
+$(phony run): $(TARGET).flash
+	@&echo You can exit qemu with C-a x (C-a a x under screen)
+	qemu-system-arm -M verdex -pflash $(input) -nographic -m 289
+
+ifdef RAMDISK
+$(TARGET).flash: $(TARGET).uimage $(ROOT)/downloads/u-boot-verdex-600-r1604.bin $(EMBRYODIR)/$(ARCH)/$(MACH)/u-boot-ramdisk.env $(RAMDISK).uimage
+	&rm -f $(output)
+	dd of=$(output) bs=4k count=8k if=/dev/zero
+	dd of=$(output) bs=4k conv=notrunc if=$(ROOT)/downloads/u-boot-verdex-600-r1604.bin
+	dd of=$(output) bs=4k conv=notrunc seek=63 if=$(EMBRYODIR)/$(ARCH)/$(MACH)/u-boot-ramdisk.env
+	dd of=$(output) bs=4k conv=notrunc seek=64 if=$(input)
+	dd of=$(output) bs=4k conv=notrunc seek=1024 if=$(RAMDISK).uimage
+else
+$(TARGET).flash: $(TARGET).uimage $(ROOT)/downloads/u-boot-verdex-600-r1604.bin $(EMBRYODIR)/$(ARCH)/$(MACH)/u-boot.env
+	&rm -f $(output)
+	dd of=$(output) bs=4k count=8k if=/dev/zero
+	dd of=$(output) bs=4k conv=notrunc if=$(ROOT)/downloads/u-boot-verdex-600-r1604.bin
+	dd of=$(output) bs=4k conv=notrunc seek=63 if=$(EMBRYODIR)/$(ARCH)/$(MACH)/u-boot.env
+	dd of=$(output) bs=4k conv=notrunc seek=64 if=$(input)
+endif
+
+endif
