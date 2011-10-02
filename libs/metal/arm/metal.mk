5b3f595bef15f7bcc20fa0e2a64a89a5fcdecc00
Torne Wuff <torne@wolfpuppy.org.uk>
2011-10-02 01:45:30 +0100

    Fix broken wildcard in initrd.zip creation.
    ** only matches directories so initrd.zip was not being rebuild when files
    changed. Replace with **/* to match all files.

--- libs/metal/arm/metal.mk
+++ libs/metal/arm/metal.mk
@@ -2,4 +2,4 @@ EXTMODULES += $(METALDIR)/$(ARCH)/$( _metalcpu_coproc.o _metalcpumodule.o)
 FREEZEDIRS += $(METALDIR)/$(ARCH)/frozen
 FREEZEDEPS += $(METALDIR)/$(ARCH)/frozen/**/*.py
 INITRDDIRS += $(METALDIR)/$(ARCH)/initrd
-INITRDDEPS += $(METALDIR)/$(ARCH)/initrd/**
+INITRDDEPS += $(METALDIR)/$(ARCH)/initrd/**/*
--- libs/metal/metal.mk
+++ libs/metal/metal.mk
@@ -2,6 +2,6 @@ METALDIR := $(ROOT)/libs/metal
 
 EXTMODULES += $(METALDIR)/_metalmemmodule.o
 INITRDDIRS += $(METALDIR)/initrd
-INITRDDEPS += $(METALDIR)/initrd/**
+INITRDDEPS += $(METALDIR)/initrd/**/*
 
 include $(METALDIR)/$(ARCH)/metal.mk
