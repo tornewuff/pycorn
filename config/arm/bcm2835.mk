8870c9fdf15a72ded3e49d40213f61ea409221cd
Torne Wuff <torne@wolfpuppy.org.uk>
2012-11-10 11:18:50 +0000

    Build with -mno-unaligned-access on bcm2835.
    This fixes the startup issue: gcc was generating unaligned halfword
    accesses because of -mcpu=arm1176. For now, disable unaligned accesses
    even though the CPU can support it, because it's useful to catch porting
    errors.

--- config/arm/bcm2835.mk
+++ config/arm/bcm2835.mk
@@ -1,2 +1,2 @@
-CFLAGS += -mcpu=arm1176jzf-s -mtune=arm1176jzf-s
-PYCFLAGS += -mcpu=arm1176jzf-s -mtune=arm1176jzf-s
+CFLAGS += -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mno-unaligned-access
+PYCFLAGS += -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mno-unaligned-access
